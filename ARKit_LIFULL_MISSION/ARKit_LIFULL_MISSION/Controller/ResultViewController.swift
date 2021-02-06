86c1e91752968b99d6ea3e2734f1912b023365e1
Hiroki Ikeuchi <pondau1024@gmail.com>
2021-02-06 17:58:34 +0900

    ダミーの画像を出力

--- ARKit_LIFULL_MISSION/ARKit_LIFULL_MISSION/Controller/ResultViewController.swift
+++ ARKit_LIFULL_MISSION/ARKit_LIFULL_MISSION/Controller/ResultViewController.swift
@@ -11,6 +11,15 @@ class ResultViewController: UIViewController {
 
     // MARK: - Properties
 
+    private let imageView: UIImageView = {
+        let iv = UIImageView()
+        iv.contentMode = .scaleAspectFit
+        iv.clipsToBounds = true
+        iv.backgroundColor = .lightGray
+        iv.layer.cornerRadius = 5
+        return iv
+    }()
+
     // MARK: - Lifecycle
 
     override func viewDidLoad() {
@@ -24,7 +33,26 @@ class ResultViewController: UIViewController {
     // MARK: - Helpers
 
     func configureUI() {
+
         view.backgroundColor = .lifullBrandColor
+
+        view.addSubview(imageView)
+        view.setDimensions(width: 600, height: 600)
+        imageView.center(inView: view)
+
+        let resultImageView = ResultImageView(dotCoordinates: [
+            Coordinate(Float.random(in: -10...10), Float.random(in: -10...10)),
+            Coordinate(Float.random(in: -10...10), Float.random(in: -10...10)),
+            Coordinate(Float.random(in: -10...10), Float.random(in: -10...10)),
+            Coordinate(Float.random(in: -10...10), Float.random(in: -10...10))
+        ])
+        resultImageView.frame.size = CGSize(width: 1024, height: 1024)
+
+        guard let image = resultImageView.convertToImage() else {
+            return
+        }
+
+        imageView.image = image
     }
 
 }
